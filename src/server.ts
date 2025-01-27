fbb90908056952f47cfe9905ff17f0c4c187692f
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2025-01-28 00:04:40 +0100

    add workaround for SSR server

--- src/server.ts
+++ src/server.ts
@@ -1,57 +1,61 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine, isMainModule } from '@angular/ssr/node';
+import {APP_BASE_HREF} from '@angular/common';
+import {CommonEngine, isMainModule} from '@angular/ssr/node';
 import express from 'express';
-import { dirname, join, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import {dirname, join, resolve} from 'node:path';
+import {fileURLToPath} from 'node:url';
 import bootstrap from './main.server';
 
-const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-const browserDistFolder = resolve(serverDistFolder, '../browser');
-const indexHtml = join(serverDistFolder, 'index.server.html');
+export function app() {
+  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+  const browserDistFolder = resolve(serverDistFolder, '../browser');
+  const indexHtml = join(serverDistFolder, 'index.server.html');
 
-const app = express();
-const commonEngine = new CommonEngine();
+  const server = express();
+  const commonEngine = new CommonEngine();
 
-/**
- * Example Express Rest API endpoints can be defined here.
- * Uncomment and define endpoints as necessary.
- *
- * Example:
- * ```ts
- * app.get('/api/**', (req, res) => {
- *   // Handle API request
- * });
- * ```
- */
+  /**
+   * Example Express Rest API endpoints can be defined here.
+   * Uncomment and define endpoints as necessary.
+   *
+   * Example:
+   * ```ts
+   * app.get('/api/**', (req, res) => {
+   *   // Handle API request
+   * });
+   * ```
+   */
 
-/**
- * Serve static files from /browser
- */
-app.get(
-  '**',
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html'
-  }),
-);
+  /**
+   * Serve static files from /browser
+   */
+  server.get(
+    '**',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+      index: 'index.html'
+    }),
+  );
 
-/**
- * Handle all other requests by rendering the Angular application.
- */
-app.get('**', (req, res, next) => {
-  const { protocol, originalUrl, baseUrl, headers } = req;
+  /**
+   * Handle all other requests by rendering the Angular application.
+   */
+  server.get('**', (req, res, next) => {
+    const {protocol, originalUrl, baseUrl, headers} = req;
 
-  commonEngine
-    .render({
-      bootstrap,
-      documentFilePath: indexHtml,
-      url: `${protocol}://${headers.host}${originalUrl}`,
-      publicPath: browserDistFolder,
-      providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-    })
-    .then((html) => res.send(html))
-    .catch((err) => next(err));
-});
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: browserDistFolder,
+        providers: [{provide: APP_BASE_HREF, useValue: baseUrl}],
+      })
+      .then((html) => res.send(html))
+      .catch((err) => next(err));
+  });
+
+  return server;
+}
 
 /**
  * Start the server if this module is the main entry point.
@@ -59,7 +63,9 @@ app.get('**', (req, res, next) => {
  */
 if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
-  app.listen(port, () => {
+  const server = app();
+
+  server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
